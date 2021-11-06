b1251da9d17004160ba9d028ad0c79e42aa4b780
dragonfly95 <nosignal95@gmail.com>
2021-11-06 22:18:42 +0900

    z

--- src/main/java/com/example/jpa101/board/BoardController.java
+++ src/main/java/com/example/jpa101/board/BoardController.java
@@ -3,6 +3,11 @@ package com.example.jpa101.board;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.jpa101.board.domain.BoardEntity;
+import com.example.jpa101.board.domain.CommentEntity;
+import com.example.jpa101.board.repository.BoardRepository;
+import com.example.jpa101.board.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class BoardController {
 
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
     @RequestMapping("")
     private ResponseEntity<Map<String, String>> hello() {
 
         Map<String, String> map = new HashMap<>();
         map.put("hello", "world");
+
+
+        BoardEntity board = BoardEntity.builder()
+                .title("hello world")
+                .build();
+
+        CommentEntity comment = CommentEntity.builder()
+                .text1("text1")
+                .text2("text2")
+                .build();
+
+
+        BoardEntity board1 = boardRepository.save(board);
+        comment.setBoard(board1);
+        CommentEntity comment1 = commentRepository.save(comment);
+        board.addComment(comment);
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
